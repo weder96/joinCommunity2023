14a8c67d38b72904597b970ae29e27492a4b7b61
weder96 <weder96@gmail.com>
2023-09-22 18:40:14 -0300

    :rocket: cli api gateway

--- learning/10_ApiGateway/app.py
+++ learning/10_ApiGateway/app.py
@@ -1,28 +1,11 @@
 import json
 
 def lambda_handler(event, context):  
-    print('Received event:',json.dumps(event))
+    print(event)
     
-    message = 'Hello From Lambda!!!' 
+    message = 'Hello From Lambda by API Gateway CLI Commands!!!' 
     
-    match event["routeKey"]: #Python 3.10
-        case "GET /product":
-            body = "Processing Get All Products"
-    
-        case "GET /product/{id}":
-            body = "Processing Get Product Id with "+ event["pathParameters"]["id"] 
-                    
-        case "POST /product":
-            payload = event["body"]
-            body = "Processing Post Product Id with "+ payload
-                    
-        case "DELETE /product/{id}":
-            body = "Processing Delete Product Id with "+event["pathParameters"]["id"]
-                    
-        case _:
-            body = "Unsupported route: "+ event["routeKey"];
-
     return {
         'statusCode': 200,        
-        'body': json.dumps(body)
+        'body': json.dumps(message),
     }
